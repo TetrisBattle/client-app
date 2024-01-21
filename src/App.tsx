5be49581df94159bc3dfc989522c8fae2de384f9
TetrisBattle <thien5596@gmail.com>
2024-01-21 13:40:07 +0200

    Move header and footer to features

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Box } from '@thng/react'
-import { Footer } from 'components/Footer'
-import { Header } from 'components/header/Header'
+import { Footer } from 'features/Footer'
+import { Header } from 'features/header/Header'
 import { Outlet } from 'react-router-dom'
 
 export const App = () => {
